7a172d3aae02779aeb7cf74d7e1a36ed9588d213
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-15 15:35:53 +0530

    Delete Context added

--- frontend/src/components/products/Product.jsx
+++ frontend/src/components/products/Product.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Products.css'
 import trash from '../../assets/delete.png'
+import { useProductsContext } from '../../hooks/UseProductsContext';
 
 const Product = ({product}) => {
-  
 
+  const { dispatch } = useProductsContext()
   const handleClick = async()=>{
-    await fetch(`http://localhost:3000/api/products/${product._id}`, {method: 'DELETE'})
+    const response = await fetch(`http://localhost:3000/api/products/${product._id}`, 
+      {method: 'DELETE'})
     .then(console.log("Product deleted"))
+    const data = response.json()
+    if(response.ok){
+      dispatch({type: 'DELETE_PRODUCTS', payload: data})
+    }
   }
   return (
     <div className='product'>
