218edc944d064e53f3e093a4ac1dc95ca37afd94
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-15 20:37:39 +0530

    Backend Login Created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,7 +6,14 @@ export const createToken = (_id) =>{
 }
 
 export const loginUser = async (req, res) => {
-  res.json({ message: "Login user" })
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({email, token});
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 export const signupUser = async (req, res) => {
   const { email, password } = req.body;
