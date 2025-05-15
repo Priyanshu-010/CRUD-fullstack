7e40f3e07dddbb00cb0b0e0a80d3699301ef1df7
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-15 16:36:36 +0530

    Validation Added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt"
+import validator from "validator"
 
 const userSchema = mongoose.Schema({
   email: {
@@ -16,6 +17,19 @@ const userSchema = mongoose.Schema({
 })
 
 userSchema.statics.signup = async function (email, password) {
+
+  if(!email || !password){
+    throw Error('All fields must be filled');
+  }
+
+  if(!validator.isEmail(email)){
+    throw Error('Email is not valid');
+  }
+
+  if(!validator.isStrongPassword(password)){
+    throw Error('Password is not strong enough');
+  }
+
   const exists = await this.findOne({email});
   if(exists){
     throw Error('Email already in use');
