38768d35bf81af18b591ae14027d034dfce868d7
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-15 22:25:54 +0530

    Signup and Login Pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
+import Signup from './pages/signup/Signup.jsx'
+import Login from './pages/login/Login.jsx'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
       <div className="pages">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/login' element={<Login />}/>
         </Routes>
       </div>
     </div>
