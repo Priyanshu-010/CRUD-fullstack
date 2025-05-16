3c2ccab5a8d3bd00256d8cbe4035217e1fc533b8
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-16 17:22:10 +0530

    Full App Conpleted

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useAuthContext } from './UseAuthContext'
+import { useProductsContext } from './UseProductsContext'
 
 const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: productsDispatch } = useProductsContext(); 
+  // The ":" colon thing above is used for calling the dispatch function wiht ither name as we already have a variable above it named dispatch
   const logout = ()=>{
     localStorage.removeItem('user')
     dispatch({type: 'LOGOUT'})
+    productsDispatch({type: 'SET_PRODUCTS', payload: null})
   }
   return {logout}   
 }
