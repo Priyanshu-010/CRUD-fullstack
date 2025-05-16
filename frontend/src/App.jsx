073e7b5b645a93e21ce78395b00d928ee06275d7
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-16 16:51:47 +0530

    Routes protected based on Login or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
 import Signup from './pages/signup/Signup.jsx'
 import Login from './pages/login/Login.jsx'
+import { useAuthContext } from './hooks/UseAuthContext.jsx'
 
 function App() {
 
+  const {user} = useAuthContext();
   return (
     <div className='App'>
       <Navbar />
       <div className="pages">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/signup' element={<Signup />}/>
-          <Route path='/login' element={<Login />}/>
+          <Route path="/" element={user ?<Home /> : <Navigate to='/login'/>} />
+          <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/'/>}/>
+          <Route path='/login' element={!user ?<Login /> : <Navigate to='/'/>}/>
         </Routes>
       </div>
     </div>
