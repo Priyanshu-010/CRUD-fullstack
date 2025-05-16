30a8d32c02245e5385d2facbec415cd263c37c20
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-16 15:17:58 +0530

    Applying useEffect to set the logged in user every time the page refreshes until the user is logget out

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import useLogout  from '../../hooks/useLogout'
+import { useAuthContext } from '../../hooks/UseAuthContext'
+
 
 const Navbar = () => {
 
   const { logout } = useLogout();
+  const {user} = useAuthContext();
   const handleClick = ()=>{
     logout();  
   }
@@ -17,13 +20,14 @@ const Navbar = () => {
           <h1>Products Logo</h1>
         </Link>
         <nav>
-          <div>
+          {user && (<div>
+            <span>{user.email}</span>
             <button onClick={handleClick}>Logout</button>
-          </div>
-          <div>
+          </div>)}
+          {!user &&(<div>
             <Link to={'/login'}>Login</Link>
             <Link to={'/signup'}>Signup</Link>
-          </div>
+          </div>)}
         </nav>
       </div>
     </header>
